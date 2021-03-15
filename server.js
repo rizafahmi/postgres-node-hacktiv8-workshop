7feb898ec503a03e2b72f9741a909541c65dcded
Riza Fahmi <rizafahmi@gmail.com>
2021-03-15 07:43:40 +0700

    Using knex

--- server.js
+++ server.js
@@ -1,80 +1,104 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const db = require("./lib/pg/runner.js");
+const db = require('./lib/pg/runner.js');
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'riza',
+    password: 'riza',
+    database: 'chats'
+  }
+});
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/", function (req, res) {
-  res.json({ status: "OK" });
+app.get('/', function(req, res) {
+  res.json({ status: 'OK' });
 });
 
-app.get("/api/users", async function (req, res) {
+app.get('/api/users', async function(req, res) {
   try {
-    const users = await db.query(`SELECT * FROM users LIMIT 100;`);
+    const users = await knex.select('*').from('users');
     res.status(200).json({
-      status: "OK",
-      users,
+      status: 'OK',
+      users
     });
   } catch (error) {
     res.status(500).json({
-      status: "KO",
-      error,
+      status: 'KO',
+      error
     });
   }
 });
 
-app.post("/api/users", async function (req, res) {
-  const { username, email, full_name } = req.body;
+app.get('/api/users/:user_id', async function(req, res) {
   try {
-    await db.execute(
-      `INSERT INTO users (username, email, full_name, created_on) VALUES ($1, $2, $3, NOW())`,
-      [username, email, full_name]
-    );
-    res.status(201).json({ status: "OK" });
+    const user = await knex
+      .select('*')
+      .from('users')
+      .where({ user_id: req.params.user_id });
+    res.status(200).json({
+      status: 'OK',
+      user
+    });
   } catch (error) {
-    res.status(501).json({ status: "KO", error });
+    res.status(500).json({
+      status: 'KO',
+      error
+    });
   }
 });
 
-app.put("/api/users/:user_id", async function (req, res) {
+app.post('/api/users', async function(req, res) {
+  const { username, email, full_name } = req.body;
   try {
-    const keys = Object.keys(req.body);
-    const values = Object.values(req.body);
-    let sql = "UPDATE users SET ";
-    for (let i = 0; i < keys.length; i++) {
-      sql += `${keys[i]}=$${i + 1},`;
-    }
-    sql = sql.slice(0, -1);
-    sql += ` WHERE user_id=${req.params.user_id} RETURNING *`;
-    const result = await db.one(`${sql};`, values);
+    const data = await knex('users')
+      .insert({ username, email, full_name, created_on: 'NOW()' })
+      .returning('*');
+    res.status(201).json({ status: 'OK', data });
+  } catch (error) {
+    console.log(error);
+    res.status(501).json({ status: 'KO', error });
+  }
+});
+
+app.put('/api/users/:user_id', async function(req, res) {
+  try {
+    const result = await knex('users')
+      .update({ last_login: 'NOW()' })
+      .where({ user_id: req.params.user_id })
+      .returning('*');
     res.status(202).json({
-      status: "OK",
-      result,
+      status: 'OK',
+      result
     });
   } catch (error) {
     res.status(502).json({
-      status: "KO",
-      error,
+      status: 'KO',
+      error: error.toString()
     });
   }
 });
 
-app.delete("/api/users/:user_id", async function (req, res) {
+app.delete('/api/users/:user_id', async function(req, res) {
   try {
-    await db.execute(`DELETE FROM users WHERE user_id=$1`, req.params.user_id);
+    await knex('users')
+      .where({ user_id: req.params.user_id })
+      .del();
     res.status(200).json({
-      status: "OK",
+      status: 'OK'
     });
   } catch (error) {
     res.status(500).json({
-      status: "KO",
-      error,
+      status: 'KO',
+      error: error.toString()
     });
   }
 });
 
-app.listen(3456, function () {
+app.listen(3456, function() {
   console.log(`Server running at port 3456`);
 });
